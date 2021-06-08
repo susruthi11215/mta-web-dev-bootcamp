402beebecbe15055de3733af40158166afc3c7c0
susruthi11215 <85492000+susruthi11215@users.noreply.github.com>
2021-06-08 21:17:00 +0530

    Add files via upload

---
+++ ass12.js
@@ -0,0 +1,9 @@
+<script>
+function changecolor(){
+	document.getElementById('change').onclick=changecolor;
+	function changecolor(){
+		document.body.style.color="red";
+		return false;
+	}
+
+</script>
